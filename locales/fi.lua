45a0f006a8d29732c34b824604c40c3ae45a72eb
Blooper <122776166+Blo0per@users.noreply.github.com>
2023-06-10 22:08:36 +0300

    Update fi.lua
    Update fi.lua

--- locales/fi.lua
+++ locales/fi.lua
@@ -3,10 +3,10 @@ Locales['fi'] = {
   ['yes'] = 'Kyllä',
   ['no'] = 'Ei',
   ['not_enough_money'] = 'Sinulla ei ole tarpeeksi rahaa',
-  ['press_menu'] = 'Paina [E] avataksesi valikko',
+  ['press_menu'] = 'Paina [E] avataksesi vaatekauppa',
   ['clothes'] = 'Vaatekauppa',
   ['you_paid'] = 'Sinä maksoit $%s',
-  ['save_in_dressing'] = 'Haluatko asulle nimen?',
+  ['save_in_dressing'] = 'Haluatko antaa asulle nimen?',
   ['name_outfit'] = 'Asun nimi',
   ['saved_outfit'] = 'Asu on tallennettu!',
 }
