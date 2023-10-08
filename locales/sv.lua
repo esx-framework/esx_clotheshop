520ac341a0614f3439ba21c552558c3e6dd17d92
Dannezzzan <123192307+Dannezzzan@users.noreply.github.com>
2023-10-08 12:23:48 +0200

    Create sv.lua
    Swedish esx_clotheshop translation

---
+++ locales/sv.lua
@@ -0,0 +1,12 @@
+Locales['sv'] = {
+    ['valid_this_purchase'] = 'Vill du köpa detta?',
+    ['yes'] = 'Ja',
+    ['no'] = 'Nej',
+    ['not_enough_money'] = 'Du har inte tillräckligt med pengar',
+    ['press_menu'] = 'Tryck [E] för att öppna klädaffären.',
+    ['clothes'] = 'Klädaffär',
+    ['you_paid'] = 'Du betalade %skr',
+    ['save_in_dressing'] = 'Vill du spara denna outfit till din fastighet?',
+    ['name_outfit'] = 'Outfit namn',
+    ['saved_outfit'] = 'Outfiten är sparad!',
+  }
