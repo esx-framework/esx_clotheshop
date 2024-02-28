59d5185a201c05acd244ec11124455a258ce3c09
TitanHaxz <titanhaxz@gmail.com>
2024-02-29 00:04:48 +0300

    Create tr.lua

---
+++ locales/tr.lua
@@ -0,0 +1,12 @@
+Locales['tr'] = {
+  ['valid_this_purchase'] = 'Değişiklikleri onaylıyor musun?',
+  ['yes'] = 'Evet',
+  ['no'] = 'Hayır',
+  ['not_enough_money'] = 'Yeterli paran yok',
+  ['press_menu'] = 'Kıyafet Mağazasına erişmek için ~b~[E]~s~[E] tuşuna bas.',
+  ['clothes'] = 'Kıyafet Mağazası',
+  ['you_paid'] = 'Ödeme yaptın: $%s',
+  ['save_in_dressing'] = 'Kıyafeti dolabınıza kaydetmek ister misin?',
+  ['name_outfit'] = 'Kıyafete bir isim ver',
+  ['saved_outfit'] = 'Kıyafet kaydedildi!',
+}
